f68af3b77431765329edd41207ec3912c8d9f4cc
Victor Swaroop <victorswaroop2002@gmail.com>
2022-07-26 08:35:22 +0000

    updated level1-2

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -18,6 +18,9 @@ def checkseq(arr, index):
 @cocotb.test()
 async def test_seq_bug1(dut):
     """Test for seq detection """
+    '''
+    Test for the input sequence [1,0,1,0,1,1]
+    '''
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
@@ -28,12 +31,7 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
-    # output seq_seen;
-    # input inp_bit;
-    # input reset;
-    # input clk;
-    # inputseq = [0, 1, 0, 1, 1, 0, 1, 1]
-    inputseq = [1,0,1,1,0,1,1]
+    inputseq = [1,0,1,0,1,1]
     for curindex, i in enumerate(inputseq):
         dut.inp_bit.value = i
         await FallingEdge(dut.clk)
@@ -44,3 +42,29 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
     
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+    '''
+    Test for the input sequence [1,1,0,1,1]
+    '''
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    inputseq = [1,1,0,1,1]
+    for curindex, i in enumerate(inputseq):
+        dut.inp_bit.value = i
+        await FallingEdge(dut.clk)
+        
+        dut._log.info(f'inputbit={i} DUTOutput={int(dut.seq_seen.value)}')
+        assert dut.seq_seen.value == checkseq(inputseq, curindex), f"Test failed with: Index={curindex}, Expected Output={checkseq(inputseq, curindex)}, DUTOutput={int(dut.seq_seen.value)}"
+
+
+    cocotb.log.info('#### CTB: Develop your test here! ######')
